7bc9b4e2cee6ecbb912425b0c5772445290143fd
xfry <fry.andrade@gmail.com>
2017-10-11 11:54:30 -0500

    new express server created

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path    = require('path');
+const app     = new express();
+
+app.use("./public", express.static(path.join(__dirname, 'public')));
+app.set('views', './public');
+app.set("view engine", 'ejs');
+
+app.get("/", (req, res) => {
+  res.render('index');
+});
+
+app.listen(8000, ()=> {
+  console.log('the app is running at port: ', 8000);
+});
