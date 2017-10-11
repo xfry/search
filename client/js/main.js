31c455f42e6e63ebad15978c5901ee2dc7b5a20c
xfry <fry.andrade@gmail.com>
2017-10-11 15:58:07 -0500

    added verification for production environment

--- client/js/main.js
+++ client/js/main.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from './components/app';
 
+if (process.env.NODE_ENV !== 'production') {
+   console.log('Running at development');
+}
+
 ReactDOM.render(
   <App />,
   document.getElementById('main')
