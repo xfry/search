14580829d3eb30f939b86c8fdb24abc90d24fb01
xfry <fry.andrade@gmail.com>
2017-10-12 17:18:15 -0500

    added react router configuration

---
+++ client/js/config-routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Layout from './pages/layout';
+import NotFound from './pages/not-found';
+import Home from './components/home';
+import PetInfo from './components/pet-info';
+
+import { Route, IndexRoute } from 'react-router';
+
+
+const ConfigRoutes = (
+  <Route>
+    <Route path="/" component={Layout}>
+      <IndexRoute component={Home} />
+      <Route path="search" component={PetInfo} />
+    </Route>
+
+    <Route path="*" component={NotFound} />
+  </Route>
+);
+
+export default ConfigRoutes;
