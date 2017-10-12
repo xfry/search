f9130d32bf3ff909e44c53b96e05e4c6d109d941
xfry <fry.andrade@gmail.com>
2017-10-12 17:17:35 -0500

    added new component pet-info for show description about pet

---
+++ client/js/components/pet-info.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <section className="search">
+        <h1 className="search__title">Welcome to Search</h1>
+      </section>
+    )
+  }
+}
+
+export default Search;
