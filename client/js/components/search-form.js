7a531e36c6dee595186f204591c516482ac073d6
xfry <fry.andrade@gmail.com>
2017-10-12 18:07:53 -0500

    added refference for sarch form

---
+++ client/js/components/search-form.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <form className="home__search-form" ref="searchForm">
+        something to search
+      </form>
+    )
+  }
+}
+
+export default Search;
