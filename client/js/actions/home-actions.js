d6f48b721fbecfe43e2d65ce5585ab09b52ff672
xfry <fry.andrade@gmail.com>
2017-10-13 10:14:13 -0500

    added action for call json data

--- client/js/actions/home-actions.js
+++ client/js/actions/home-actions.js
@@ -2,8 +2,8 @@ import dispatcher from '../dispatcher/app-dispatcher';
 
 export const dispatchPetList = (res) => {
   if(!res.error) {
-    dispatcher.dispatch({type: "RECEIVED_PET_LIST", data: res});
+    dispatcher.dispatch({type: "RECEIVED_PET_LIST", data: JSON.parse(res)});
   } else {
-    dispatcher.dispatch({type: "ERROR_RECEIVING_PET_LIST", error: res.error});
+    dispatcher.dispatch({type: "ERROR_RECEIVING_PET_LIST", error: JSON.parse(res.error)});
   }
 };
